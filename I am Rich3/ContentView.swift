771b868b3509681ee7c81b631b040fa68eeca90a
@arnavagarwal <alnico.arnav@gmail.com>
2021-04-19 02:20:28 +0530

    First commit

---
+++ I am Rich3/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I am Rich3
+//
+//  Created by Arnav Agarwal on 10/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
